281be1471e3e7d779ed03dbb53c65d063fbc3a27
Alexander Johnston <Aliics@hotmail.com>
2021-08-12 12:18:39 +1200

    accept many timestamps

--- day-start/DayStart.java
+++ day-start/DayStart.java
@@ -1,24 +1,44 @@
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeParseException;
+import java.util.Optional;
 
-public final class DayStart {
-    public static void main(final String[] args) {
-        if (args.length < 1) {
-            System.out.println("Please provide at least one ISO date");
-            return;
-        }
+public class DayStart {
+    public static void main(String[] args) {
+        assert args.length >= 1 : "Please provide at least one ISO date or timestamp";
 
-        for (final String dateArg : args) {
+        for (final var dateArg : args) {
             try {
-                final long millis = LocalDate.parse(dateArg)
-                        .atStartOfDay(ZoneId.systemDefault())
-                        .toInstant()
-                        .toEpochMilli();
+                // Optionally forms and instant from the provided argument:
+                // 2021-01-01T00:00:00Z <- 1st branch (Some)
+                // 1609412400000        <- 2nd branch (Some)
+                // 2021-01-01           <- 3rd branch (None)
+                final Optional<Instant> optionalInstant =
+                        dateArg.contains("T") ? Optional.of(Instant.parse(dateArg)) :
+                        !dateArg.contains("-") ? Optional.of(Instant.ofEpochMilli(Long.parseLong(dateArg))) :
+                        Optional.empty();
+
+                final long millis = optionalInstant.map(instant ->
+                        // dateArg was a timestamp
+                        instant.atZone(ZoneId.systemDefault())
+                                .toLocalDate()
+                                .atStartOfDay(ZoneId.systemDefault())
+                                .toInstant()
+                                .toEpochMilli()
+                ).orElseGet(() ->
+                        // dateArg was probably a LocalDate
+                        LocalDate.parse(dateArg)
+                                .atStartOfDay(ZoneId.systemDefault())
+                                .toInstant()
+                                .toEpochMilli()
+                );
 
                 System.out.printf("%s: (%016x) %d\n", dateArg, millis, millis);
-            } catch (final DateTimeParseException e) {
+            } catch (DateTimeParseException e) {
                 System.out.printf("%s is not a valid DateTime\n", dateArg);
+            } catch (NumberFormatException e) {
+                System.out.printf("%s is not a valid timestamp\n", dateArg);
             }
         }
     }
